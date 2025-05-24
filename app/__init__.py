b6e8c9c9c4bdb28e53cd6f26f5d5d00fdec536ee
Felipe Braga <felipeleobraga@gmail.com>
2025-05-24 09:45:35 -0300

    Correção models

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from app.models.db_instance import db
-from app.models import user, donation_item  # importa os arquivos para registrar os modelos
-
-
+from app.models.user import User
+from app.models.fitness_account import FitnessAccount
+from app.models.activity import Activity
+from app.models.donation_item import DonationItem
+from app.models.donation import Donation
+from app.models.challenge import Challenge
 
 def create_app():
     app = Flask(__name__)
@@ -10,21 +13,12 @@ def create_app():
 
     db.init_app(app)
 
-    from .routes.main_routes import main
-    from .routes.donation_routes import donation
-    from .routes.fitness_routes import fitness
-    from .user import User
-    from .fitness_account import FitnessAccount
-    from .activity import Activity
-    from .donation_item import DonationItem
-    from .donation import Donation
-    from .challenge import Challenge
-
+    from routes.main_routes import main
+    from routes.donation_routes import donation
+    from routes.fitness_routes import fitness
 
     app.register_blueprint(main)
     app.register_blueprint(donation)
     app.register_blueprint(fitness)
 
     return app
-
-
