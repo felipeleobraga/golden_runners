cfe142aba9737b6e9b459dfdb4608ed886e9df76
Felipe Braga <felipeleobraga@gmail.com>
2025-05-28 14:22:12 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-SQLALCHEMY_DATABASE_URI = ""
+SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
 SQLALCHEMY_TRACK_MODIFICATIONS = False
