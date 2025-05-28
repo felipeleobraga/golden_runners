8f34d3f69d92a4f783839be4080efe7f238e5174
Felipe Braga <felipeleobraga@gmail.com>
2025-05-28 14:21:23 -0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret")
-SQLALCHEMY_DATABASE_URI = "postgresql://postgres:SZhQlNwPuOLskuBbJKGWriPwtqqfuDVq@shortline.proxy.rlwy.net:29880/railway"
+SQLALCHEMY_DATABASE_URI = ""
 SQLALCHEMY_TRACK_MODIFICATIONS = False
